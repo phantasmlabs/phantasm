a4df34b782999d36dca9f24b878cdc17eed127fd
Edwin Kys <itsedwinkys@gmail.com>
2024-10-16 18:26:06 -0500

    feat: add connection and approval id type

--- src/types.rs
+++ src/types.rs
@@ -1,9 +1,32 @@
+use tokio::sync::mpsc::UnboundedSender;
+use tokio_tungstenite::tungstenite::Message;
 use uuid::Uuid;
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
 pub struct ConnectionID(Uuid);
 
 impl ConnectionID {
+    pub fn new() -> Self {
+        Self(Uuid::new_v4())
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct Connection {
+    pub sender: UnboundedSender<Message>,
+    pub load: usize,
+}
+
+impl Connection {
+    pub fn new(sender: UnboundedSender<Message>) -> Self {
+        Self { sender, load: 0 }
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+pub struct ApprovalID(Uuid);
+
+impl ApprovalID {
     pub fn new() -> Self {
         Self(Uuid::new_v4())
     }
