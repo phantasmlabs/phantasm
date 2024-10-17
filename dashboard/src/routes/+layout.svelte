27f02fff6974798e23e20f8f0cec1ea312599596
Edwin Kys <itsedwinkys@gmail.com>
2024-10-17 16:49:19 -0500

    feat(dashboard): install fontsource

---
+++ dashboard/src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+<script>
+  import "@fontsource/cabin/400.css"
+  import "@fontsource/cabin/700.css"
+</script>
+
+<slot />
+
+<style>
+  :global(body) {
+    font-family: "Cabin", sans-serif;
+  }
+</style>
