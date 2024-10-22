72aad18abcc8fe199a39ac9d2d30aa7a91cc4243
Edwin Kys <itsedwinkys@gmail.com>
2024-10-21 23:27:10 -0500

    feat: add heartbeat method in client

--- clients/python/phantasmpy/client.py
+++ clients/python/phantasmpy/client.py
@@ -1,15 +1,16 @@
 import grpc
+from google.protobuf.empty_pb2 import Empty
+from stubs import receiver_pb2 as protos
 from stubs.receiver_pb2_grpc import ReceiverStub
 
 
 class Phantasm:
     def __init__(self, host: str = "localhost", port: int = 2505):
-        channel = grpc.insecure_channel("localhost:50051")
+        channel = grpc.insecure_channel(f"{host}:{port}")
         self.connection = ReceiverStub(channel)
 
     def heartbeat(self):
-        response = self.connection.Heartbeat()
-        return response
+        return self.connection.Heartbeat(request=Empty())
 
     def get_approval(self):
         pass
